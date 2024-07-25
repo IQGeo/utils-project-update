2a27be4eeab433ef43c8b1ef3a023594bc090760
Luis Camacho <luis.camacho@iqgeo.com>
2024-07-25 18:17:25 +0100

    pull: include entrypoint and oidc conf files

--- src/pull/index.js
+++ src/pull/index.js
@@ -37,6 +37,10 @@ const INCLUDE_FILES = [
     '.devcontainer/docker-compose.yml',
     '.devcontainer/.env.example',
     '.devcontainer/devcontainer.json',
+    '.devcontainer/entrypoint.d/270_adjust_oidc_conf.sh',
+    '.devcontainer/entrypoint.d/600_init_db.sh',
+    '.devcontainer/entrypoint.d/850_fetch.sh',
+    '.devcontainer/devserver_config/oidc/conf.json',
     '.devcontainer/remote_host/devcontainer.json',
     '.devcontainer/remote_host/dockerfile',
     '.devcontainer/remote_host/docker-compose.yml',
@@ -45,7 +49,10 @@ const INCLUDE_FILES = [
     'deployment/dockerfile.appserver',
     'deployment/dockerfile.tools',
     'deployment/docker-compose.yml',
-    'deployment/.env.example'
+    'deployment/.env.example',
+    'deployment/entrypoint.d/270_adjust_oidc_conf.sh',
+    'deployment/entrypoint.d/600_init_db.sh',
+    'deployment/appserver_config/oidc/conf.json'
     // Custom content of shell files should be in separate files
 ];
 
