1b5cdf971c855941896b2fe29d32079739e9886b
Daniel Knights <59598622+Daniel-Knights@users.noreply.github.com>
2025-03-07 12:21:48 +0000

    ci(release): add confirmation step before publishing

--- scripts/release.js
+++ scripts/release.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { createRequire } from 'node:module';
+import readline from 'node:readline';
 
 import { ensureCleanWorkingTree, run } from '../src/helpers.js';
 
@@ -17,6 +18,23 @@ ensureCleanWorkingTree(process.cwd());
 run('npm', ['run', 'lint']);
 run('npm', ['version', args[0]]);
 run('npx', ['changenog']);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Review changes then press enter to continue...');
+
+await new Promise(res => {
+    rl.on('line', () => {
+        rl.close();
+
+        // @ts-expect-error - JSDoc typing for this looks gross
+        res();
+    });
+});
+
 run('git', ['add', '.']);
 
 const pkg = require('../package.json');
